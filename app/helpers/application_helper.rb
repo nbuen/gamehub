99cbea4dd1ad4cce2eaf607570c6186e946ebdd4
nikko <nikko.buen@yahoo.com>
2018-11-14 16:48:48 +0000

    Added cart_item controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+	def current_order
+		if !session[:shopping_cart_id].nil?
+			ShoppingCart.find(session[:shopping_cart_id])
+		else
+			ShoppingCart.new
+		end
+	end
 end
