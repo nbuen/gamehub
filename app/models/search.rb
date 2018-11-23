1a4f5d6c52e698343b7b56089d4b68946411ad8a
nikko <nikko.buen@yahoo.com>
2018-11-23 20:32:13 +0000

    Upgraded ruby/fixed search

--- app/models/search.rb
+++ app/models/search.rb
@@ -4,11 +4,12 @@ class Search < ApplicationRecord
 		products = Product.all
 		categories = Category.all
 
-		categ_ar = categories.where(["name LIKE ?","%#{category}%"]).pluck(:id) if category.present?
-		category = Category.find(categ_ar[0]) if category.present?
+		categ_ar = categories.where(["name LIKE ?","%#{category}%"]) if category.present?
+		categ_id = categ_ar.ids if category.present?
+		categ = Category.find(categ_id[0]) if category.present?
+		products = categ.products if category.present?
 
-		products = products.where(["name LIKE ?","%#{keywords}%"]) if keywords.present?
-		products = category.products if category.present?
+		products = products.where(["name LIKE ?","%#{keywords}%"]) if keywords.present?		
 		products = products.where(["price >= ?", min_price]) if min_price.present?
 		products = products.where(["price <= ?", max_price]) if max_price.present?
 
